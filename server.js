32acfbfcf72c7251088e13e97105ff68385c136e
Fabio Ferreira Reis <64235246+FFReis23@users.noreply.github.com>
2025-05-04 13:07:02 -0300

    server.js

--- server.js
+++ server.js
@@ -1,32 +1,41 @@
-// server.js
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const axios = require("axios");
+const nodemailer = require("nodemailer");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "7757093107:AAHFmLastbU0lkV5f2AdeiNXHoxckQsJVy4"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "1276935257"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+// Configuração do transporter do Nodemailer
+const transporter = nodemailer.createTransport({
+  service: "gmail",  // Ou outro serviço de e-mail
+  auth: {
+    user: "machshev.reis@gmail.com", // Substitua pelo seu e-mail
+    pass: "$$Yeshua$$31$$Reb",   // Substitua pela sua senha de e-mail
+  },
+});
+
+const destinatarioEmail = "machshev.reis@exemplo.com"; // O e-mail para onde a localização será enviada
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
 
-  const message = `A localização do usuário é:\nLatitude: ${latitude}\nLongitude: ${longitude}\nMaps: ${maps}`;
+  const message = {
+    from: "machshev.reis@gmail.com", // O e-mail de envio
+    to: destinatarioEmail,
+    subject: "Localização do Usuário",
+    text: `A localização do usuário é:\nLatitude: ${latitude}\nLongitude: ${longitude}\nLink do Google Maps: ${maps}`,
+  };
 
   try {
-    // Envia a localização para o Telegram
-    await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
-      chat_id: TELEGRAM_CHAT_ID,
-      text: message,
-    });
+    // Envia o e-mail
+    await transporter.sendMail(message);
 
     res.status(200).json({ success: true });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ success: false, message: "Erro ao enviar a localização para o Telegram." });
+    res.status(500).json({ success: false, message: "Erro ao enviar a localização por e-mail." });
   }
 });
 
